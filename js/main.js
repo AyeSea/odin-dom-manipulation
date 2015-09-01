99069f4151cdd790e311cbaa0d1375df89b6ade9
Andrew Chung <andrew.chung159@gmail.com>
2015-09-01 00:17:22 -0400

    complete addCarousel and refactor #content append code

--- js/main.js
+++ js/main.js
@@ -5,15 +5,24 @@ $(document).ready(function() {
 	addFooter();
 });
 
+function addToContent(childElement) {
+	$('#content').append(childElement);
+};
+
 function addHeader() {
 	var header = $("<header class='container-fluid site-header'></header>");
 	var logo = $("<h1 id='logo'></logo>").text('Lucia Pizza');
 
-	$('#content').append(header);
+	addToContent(header);
 	header.append(logo);
 };
 
 function addCarousel() {
+	var carousel = $("<section class='container-fluid carousel'></section>");
+	var slide1 = $("<div class='slide current-slide' id='slide-1'></div>");
+
+	addToContent(carousel);
+	carousel.append(slide1);
 };
 
 function addAboutSection() {
